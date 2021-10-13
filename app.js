8f5d8f1218e894db9b63f005d23ef8c04c965fb4
nelsonbi <55667233+nelsonbi@users.noreply.github.com>
2021-10-13 20:50:08 +0200

    Boats initialized in datastore

--- app.js
+++ app.js
@@ -1,93 +1,131 @@
-// Copyright 2017 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// [START gae_flex_datastore_app]
-'use strict';
-
 const express = require('express');
-const crypto = require('crypto');
-
 const app = express();
-app.enable('trust proxy');
 
-// By default, the client will authenticate using the service account file
-// specified by the GOOGLE_APPLICATION_CREDENTIALS environment variable and use
-// the project specified by the GOOGLE_CLOUD_PROJECT environment variable. See
-// https://github.com/GoogleCloudPlatform/google-cloud-node/blob/master/docs/authentication.md
-// These environment variables are set automatically on Google App Engine
-const {Datastore} = require('@google-cloud/datastore');
+const { Datastore } = require('@google-cloud/datastore');
+const bodyParser = require('body-parser');
 
-// Instantiate a datastore client
 const datastore = new Datastore();
 
-/**
- * Insert a visit record into the database.
- *
- * @param {object} visit The visit record to insert.
- */
-const insertVisit = visit => {
-  return datastore.save({
-    key: datastore.key('visit'),
-    data: visit,
-  });
-};
+const BOATS = "Boats";
+const SLIPS = "Slips"
+
+const router = express.Router();
+
+app.use(bodyParser.json());
+
+function fromDatastore(item) {
+    item.id = item[Datastore.KEY].id;
+    return item;
+}
+
+/* ------------- Begin Lodging Model Functions ------------- */
+function post_boat(name, type, length) {
+    var key = datastore.key(BOATS);
+    const new_boat = { "name": name, "type": type, "length": length };
+    return datastore.save({ "key": key, "data": new_boat }).then(() => { return key });
+}
 
 /**
- * Retrieve the latest 10 visit records from the database.
+ * The function datastore.query returns an array, where the element at index 0
+ * is itself an array. Each element in the array at element 0 is a JSON object
+ * with an entity fromt the type "Lodging".
  */
-const getVisits = () => {
-  const query = datastore
-    .createQuery('visit')
-    .order('timestamp', {descending: true})
-    .limit(10);
+function get_boats() {
+    const q = datastore.createQuery(BOATS);
+    return datastore.runQuery(q).then((entities) => {
+        // Use Array.map to call the function fromDatastore. This function
+        // adds id attribute to every element in the array at element 0 of
+        // the variable entities
+        return entities[0].map(fromDatastore);
+    });
+}
 
-  return datastore.runQuery(query);
-};
+/**
+ * This function is not in the code discussed in the video. It demonstrates how
+ * to get a single entity from Datastore using an id.
+ * Note that datastore.get returns an array where each element is a JSON object 
+ * corresponding to an entity of the Type "Lodging." If there are no entities
+ * in the result, then the 0th element is undefined.
+ * @param {number} id Int ID value
+ * @returns An array of length 1.
+ *      If a lodging with the provided id exists, then the element in the array
+ *           is that lodging
+ *      If no lodging with the provided id exists, then the value of the 
+ *          element is undefined
+ */
+function get_boat(id) {
+    const key = datastore.key([BOATS, parseInt(id, 10)]);
+    return datastore.get(key).then((entity) => {
+        if (entity[0] === undefined || entity[0] === null) {
+            // No entity found. Don't try to add the id attribute
+            return entity;
+        } else {
+            // Use Array.map to call the function fromDatastore. This function
+            // adds id attribute to every element in the array entity
+            return entity.map(fromDatastore);
+        }
+    });
+}
 
-app.get('/', async (req, res, next) => {
-  // Create a visit record to be stored in the database
-  const visit = {
-    timestamp: new Date(),
-    // Store a hash of the visitor's ip address
-    userIp: crypto
-      .createHash('sha256')
-      .update(req.ip)
-      .digest('hex')
-      .substr(0, 7),
-  };
+function put_boat(id, name, type, length) {
+    const key = datastore.key([BOATS, parseInt(id, 10)]);
+    const boat = { "name": name, "type": type, "length": length };
+    return datastore.save({ "key": key, "data": boat });
+}
 
-  try {
-    await insertVisit(visit);
-    const [entities] = await getVisits();
-    const visits = entities.map(
-      entity => `Time: ${entity.timestamp}, AddrHash: ${entity.userIp}`
-    );
-    res
-      .status(200)
-      .set('Content-Type', 'text/plain')
-      .send(`Last 10 visits:\n${visits.join('\n')}`)
-      .end();
-  } catch (error) {
-    next(error);
-  }
+function delete_boat(id) {
+    const key = datastore.key([BOATS, parseInt(id, 10)]);
+    return datastore.delete(key);
+}
+
+/* ------------- End Model Functions ------------- */
+
+/* ------------- Begin Controller Functions ------------- */
+
+router.get('/', function (req, res) {
+    const boats = get_boats()
+        .then((boats) => {
+            res.status(200).json(boats);
+        });
 });
 
+router.post('/', function (req, res) {
+    post_boat(req.body.name, req.body.type, req.body.length)
+        .then(key => { res.status(200).send('{ "id": ' + key.id + ' }') });
+});
+
+router.put('/:id', function (req, res) {
+    put_boat(req.params.id, req.body.name, req.body.type, req.body.length)
+        .then(res.status(200).end());
+});
+
+router.delete('/:id', function (req, res) {
+    delete_boat(req.params.id).then(res.status(200).end())
+});delete_boat
+/**
+ * This route is not in the file discussed in the video. It demonstrates how to
+ * get a single lodging from Datastore using the provided id and also how to 
+ * determine when no lodging exists with that ID.
+ */
+router.get('/:id', function (req, res) {
+    get_boat(req.params.id)
+        .then(boat => {
+            if (boat[0] === undefined || boat[0] === null) {
+                // The 0th element is undefined. This means there is no lodging with this id
+                res.status(404).json({ 'Error': 'No lodding exists with this id' });
+            } else {
+                // Return the 0th element which is the lodging with this id
+                res.status(200).json(boat[0]);
+            }
+        });
+});
+
+/* ------------- End Controller Functions ------------- */
+
+app.use('/boats', router);
+
+// Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
 });
-// [END gae_flex_datastore_app]
-
-module.exports = app;
